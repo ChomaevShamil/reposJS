fafdaf40e5bc616f67349c588aa4715f6aa069b2
Shamil Chomaev <chomaev.exceedteam@gmail.com>
2022-02-03 17:27:44 +0300

    izm i neizm tipy dannih

--- data.js
+++ data.js
@@ -6,3 +6,18 @@ typeof true
 typeof "Hello"
 typeof Symbol("y")
 typeof null 
+
+
+//изменяемые и незменяемые типы данных
+let gof = "20";
+let tof = gof;
+gof = "21"
+alert(gof == tof)
+
+const Days = {
+    friday: 5,
+    satarday: 6,
+    sunday: 7
+}
+Days.monday = 1
+console.log(Days)
