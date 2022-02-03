0de23d7ad9dbe118e9c742b4a6d7a49c7714326a
Shamil Chomaev <chomaev.exceedteam@gmail.com>
2022-02-03 12:37:53 +0300

    usl operatori

--- new.js
+++ new.js
@@ -1,9 +1,18 @@
+// var, let, const
 const a = {
     b: 1,
     y: 2
 }
-const isA = a 
+const isA = a
 isA.c = "abc"
 console.log(isA)
-let t 
-t = 10 
+let t
+t = 10
+
+// синтаксис усл операторов
+let A;
+if (confirm("Are you over 18?")) {
+    A = prompt("What's your name?", A)
+    alert("Welcome, " + A);
+} else
+    alert("Access denied");
